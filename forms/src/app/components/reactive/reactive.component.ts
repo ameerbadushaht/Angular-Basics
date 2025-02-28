3aa7cf1bcb5aa48649b70e1fadf21de08e9f0c84
Ameer Badushah T <ameerbadushat123@gmail.com>
2025-02-28 15:38:08 +0530

    Reactive forms - FormArray

--- forms/src/app/components/reactive/reactive.component.ts
+++ forms/src/app/components/reactive/reactive.component.ts
@@ -1,6 +1,5 @@
-import { group } from '@angular/animations';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators, Validator, AbstractControl, ValidationErrors } from '@angular/forms';
+import { FormControl, FormGroup, Validators, Validator, AbstractControl, ValidationErrors, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -13,11 +12,33 @@ userForm= new FormGroup({
     name: new FormControl('',[Validators.required,Validators.minLength(3)]),
     email: new FormControl('',[Validators.required,Validators.email]),
     password: new FormControl('',[Validators.required]),
-    
+    skills: new FormArray([
+        new FormGroup({
+          name: new FormControl('', Validators.required)
+        })
+      ]) 
   })
+ 
+
+   get skills() {
+    return this.userForm.get('skills') as FormArray;
+  }
+
+
+  addSkill() {
+    this.skills.push(
+      new FormGroup({
+        name: new FormControl('', Validators.required)
+      })
+    );
+  }
+
+  removeSkill(index: number) {
+    this.skills.removeAt(index);
+  }
 
   onSubmit(){
-    console.log(this.userForm);
+    console.log(this.userForm.value);
   }
 
 }
